44b713b3bb077c34e5263714f4014250b513831c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2009-04-02 20:50:35 +0000

    ...

--- lib/Test/Mock/Class.pm
+++ lib/Test/Mock/Class.pm
@@ -31,7 +31,119 @@ our $VERSION = '0.01';
 use Moose;
 
 
-use Test::Mock::Class::Generator;
+use constant::boolean;
+
+use Class::MOP;
+use Moose::Meta::Class;
+
+use Exception::Base 'Test::Mock::Class::Exception';
+
+use Test::Mock::Class::Base;
+use Test::Mock::Class::Inspector;
+
+
+use constant {
+    Base      => 'Test::Mock::Class::Base',
+    Exception => 'Test::Mock::Class::Exception',
+    Inspector => 'Test::Mock::Class::Inspector',
+};
+
+
+=head1 ATTRIBUTES
+
+=over
+
+=item class : Str
+
+Class to clone.
+
+=cut
+
+has 'class' => (
+    is        => 'ro',
+    isa       => 'Str',
+    predicate => 'has_class',
+);
+
+=item mock_class : Str
+
+New class name.
+
+=cut
+
+has 'mock_class' => (
+    is        => 'ro',
+    isa       => 'Str',
+    writer    => '_set_mock_class',
+    predicate => 'has_mock_class',
+);
+
+=item mock_metaclass : Moose::Meta::Class
+
+Metaclass for mocked class.
+
+=cut
+
+has 'mock_metaclass' => (
+    is        => 'ro',
+    writer    => '_set_mock_metaclass',
+);
+
+=item methods : ArrayRef[Str]
+
+List of methods which are created for mocked class.
+
+=cut
+
+has 'methods' => (
+    is        => 'ro',
+    isa       => 'ArrayRef[Str]',
+    predicate => 'has_methods',
+);
+
+=item mock_base : Str
+
+Base mock class name.
+
+=cut
+
+has 'mock_base' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => Base,
+);
+
+=item inspector : Test::Mock::Class::Inspector
+
+Reflection API for class.
+
+=cut
+
+has 'inspector' => (
+    is      => 'ro',
+    isa     => Inspector,
+    lazy    => TRUE,
+    default => sub {
+        Inspector->new( class => $_[0]->class )
+    },
+);
+
+=item mock_inspector : Test::Mock::Class::Inspector
+
+Reflection API for mocked class.
+
+=back
+
+=cut
+
+has 'mock_inspector' => (
+    is      => 'ro',
+    isa     => Inspector,
+    lazy    => TRUE,
+    default => sub {
+        Inspector->new( class => $_[0]->mock_class )
+    },
+);
 
 
 use namespace::clean -except => 'meta';
@@ -41,78 +153,80 @@ use namespace::clean -except => 'meta';
 
 =over
 
-=item generate(I<class> : Str, I<mock_class> : Str = undef, I<methods> : Array = ())
+=item generate(I<>) : ClassName
 
 Clones a class' interface and creates a mock version that can have return
 values and expectations set.
-
-=over
-
-=item I<class>
-
-Class to clone.
-
-=item I<mock_class>
-
-New class name. Default is the old name with C<::Mock>
-appended.
-
-=item I<methods>
-
-Additional methods to add beyond those in the cloned class.  Use this to
-emulate the dynamic addition of methods in the cloned class or when the class
-hasn't been written yet.
 
 =back
 
 =cut
 
 sub generate {
-    my ($self, $class, $mock_class, @methods) = @_;
-    my $generator = Test::Mock::Class::Generator->new(
-        class      => $class,
-        defined $mock_class ? (mock_class => $mock_class) : (),
-    );
-    return $generator->generate_subclass(@methods);
+    my ($self) = @_;
+
+    if ($self->has_class) {
+        Exception->throw(
+            message => [ 'Class %s does not exist', $self->class ],
+        ) unless $self->inspector->class_exists;
+    };
+    
+    if ($self->has_mock_class) {
+        Exception->throw(
+            message => [ 'Class %s already exists', $self->mock_class ],
+        ) if $self->mock_inspector->class_exists_sans_autoload;
+    };
+
+    $self->_create_class;
+
+    return $self->mock_class;
 };
 
 
-=item mock_class_partial(I<class> : Str, I<mock_class> : Str, I<methods> : Array)
+sub _create_class {
+    my ($self) = @_;
 
-Generates a version of a class with selected methods mocked only.  Inherits
-the old class and chains the mock methods of an aggregated mock object.
+    my @methods = $self->has_methods ? @{ $self->methods } : ();
 
-=over
+    my @mock_methods = do {
+        my %uniq = map { $_ => 1 }
+                   ($self->inspector->get_methods, @methods, 'new');
+        keys %uniq;
+    };
 
-=item I<class>
+    my $metaclass;
+    if ($self->has_mock_class) {
+        $metaclass = Moose::Meta::Class->create($self->mock_class);
+    }
+    else {
+        $metaclass = Moose::Meta::Class->create_anon_class;
+        $self->_set_mock_class($metaclass->name);
+    };
 
-Class to clone.
+    $self->_set_mock_metaclass($metaclass);
 
-=item I<mock_class>
+    my @metaclass_instance_roles = $self->inspector->get_metaclass_instance_roles;
+    if (@metaclass_instance_roles) {
+        Moose::Util::MetaRole::apply_metaclass_roles(
+            for_class => $self->mock_class,
+            instance_metaclass_roles => \@metaclass_instance_roles,
+        );  
+    };
 
-New class name.
+    $metaclass->superclasses(Base, $self->inspector->get_superclasses);
 
-I<methods>
+    foreach my $method (@mock_methods) {
+        next if $method eq 'meta';
+        if ($method eq 'new') {
+            $self->mock_class->mock_add_constructor($method);
+        }
+        else {
+            $self->mock_class->mock_add_method($method);
+        };
+    };
 
-Methods to be overridden with mock versions.
-
-=back
-
-=cut
-
-sub mock_class_partial {
-    my ($self, $class, $mock_class, @methods) = @_;
-    my $generator = Test::Mock::Class::Generator->new(
-        class      => $class,
-        mock_class => $mock_class
-    );
-    return $generator->generate_subclass_partial(@methods);
+    return $self;
 };
-
-
-=back
-
-=cut
 
 
 1;
@@ -127,7 +241,15 @@ sub mock_class_partial {
 [                          <<utility>>
                         Test::Mock::Class
  -----------------------------------------------------------------------
+ +class : Str
+ +mock_class : Str
+ +mock_metaclass : Moose::Meta::Class
+ +methods : ArrayRef[Str]
+ +mock_base : Str
+ +inspector : Test::Mock::Class::Inspector
+ +mock_inspector : Test::Mock::Class::Inspector
  -----------------------------------------------------------------------
+ +generate() : ClassName
  mock_class(class : Str, mock_class : Str = undef, methods : Array = ())
  mock_class_partial(class : Str, mock_class : Str, methods : Array)
                                                                         ]
@@ -136,7 +258,7 @@ sub mock_class_partial {
 
 =head1 SEE ALSO
 
-L<Moose>.
+L<Moose::Meta::Class>.
 
 =head1 BUGS
 
@@ -154,7 +276,7 @@ Based on SimpleTest, an open source unit test framework for the PHP
 programming language, created by Marcus Baker, Jason Sweat, Travis Swicegood,
 Perrick Penet and Edward Z. Yang.
 
-Copyright (c) 2009 Piotr Roszatycki E<lt>dexter@debian.orgE<gt>.
+Copyright (c) 2009 Piotr Roszatycki <dexter@cpan.org>.
 
 This program is free software; you can redistribute it and/or modify it
 under GNU Lesser General Public License.
