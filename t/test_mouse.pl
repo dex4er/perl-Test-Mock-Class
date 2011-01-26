458c5cd1d740e25e016b5b4477984468091bbaef
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2011-01-26 17:01:12 +0100

    Fixed test_*.pl scripts.

--- t/test_mouse.pl
+++ t/test_mouse.pl
@@ -1,5 +1,29 @@
 #!/usr/bin/perl
 
+use 5.006;
+
+use strict;
+use warnings;
+
+use File::Basename;
+use File::Spec;
+use Cwd;
+
+BEGIN {
+    chdir dirname(__FILE__) or die "$!";
+    chdir '..' or die "$!";
+
+    unshift @INC, map { /(.*)/; $1 } split(/:/, $ENV{PERL5LIB}) if defined $ENV{PERL5LIB} and ${^TAINT};
+
+    my $cwd = ${^TAINT} ? do { local $_=getcwd; /(.*)/; $1 } : '.';
+    unshift @INC, File::Spec->catdir($cwd, 'inc');
+    unshift @INC, File::Spec->catdir($cwd, 'lib');
+}
+
+use Test::Unit::Lite;
+
 $ENV{ANY_MOOSE} = 'Mouse';
-do 'test.pl';
-die $@ if $@;
+
+local $SIG{__WARN__} = sub { require Carp; Carp::confess(@_) };
+
+all_tests;
